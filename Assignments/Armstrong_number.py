debfc07bda428110190d49b093d832761c8800c0
kira5901 <shubhamsharma5901@gmail.com>
2020-05-18 21:56:54 +0530

    Assignment Session-2

---
+++ Assignments/Armstrong_number.py
@@ -0,0 +1,20 @@
+def order(n):
+    x=0
+    while(n!=0):
+        x+=1
+        n=n//10
+    return x
+
+def armstrong(n):
+    num=n
+    x=order(n)
+    s=0
+    while(n!=0):
+        d=n%10
+        s+=d**x
+        n=n//10
+    if num==s:
+        print("It is Armstrong number")
+    else:
+        print("Not Armstrong number")
+armstrong(int(input("Enter number of your choice")))
---
+++ Assignments/Prime_Numbers.py
@@ -0,0 +1,9 @@
+#Printing prime numbers between 100 to 300
+
+for i in range(100,301):
+    c=0
+    for j in range(1,i+1):
+        if i%j==0:
+            c+=1
+    if c==2:
+        print(i)
