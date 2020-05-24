8998302b39a1df972389063a305c0cb48e0748e1
kira5901 <shubhamsharma5901@gmail.com>
2020-05-24 11:59:26 +0530

    Weeek 1

--- Assignments/Armstrong_number.py
+++ Assignments/Armstrong_number.py
@@ -1,20 +1,28 @@
-def order(n):
-    x=0
-    while(n!=0):
-        x+=1
-        n=n//10
-    return x
+#def order(n):
+#    x=0
+#    while(n!=0):
+#        x+=1
+#        n=n//10
+#    return x
+#
+#def armstrong(n):
+#    num=n
+#    x=order(n)
+#    s=0
+#    while(n!=0):
+#        d=n%10
+#        s+=d**x
+#        n=n//10
+#    if num==s:
+#        print("It is Armstrong number")
+#    else:
+#        print("Not Armstrong number")
+#armstrong(int(input("Enter number of your choice")))
 
-def armstrong(n):
-    num=n
-    x=order(n)
-    s=0
-    while(n!=0):
-        d=n%10
-        s+=d**x
-        n=n//10
-    if num==s:
-        print("It is Armstrong number")
-    else:
-        print("Not Armstrong number")
-armstrong(int(input("Enter number of your choice")))
+class a:
+    def __init__(self,a="hello"):
+        self.a=a
+    def display(self):
+        print(self.a)
+b=a()
+b.display()
