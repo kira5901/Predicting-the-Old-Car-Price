c1f2b15ca8789a9f6c39f41e9e4304e8654b63ca
kira5901 <shubhamsharma5901@gmail.com>
2020-06-21 13:59:58 +0530

    Data Visualization Assignment

--- Assignments/Armstrong_number.py
+++ Assignments/Armstrong_number.py
@@ -1,24 +1,20 @@
-#def order(n):
-#    x=0
-#    while(n!=0):
-#        x+=1
-#        n=n//10
-#    return x
-#
-#def armstrong(n):
-#    num=n
-#    x=order(n)
-#    s=0
-#    while(n!=0):
-#        d=n%10
-#        s+=d**x
-#        n=n//10
-#    if num==s:
-#        print("It is Armstrong number")
-#    else:
-#        print("Not Armstrong number")
-#armstrong(int(input("Enter number of your choice")))
+def order(n):
+    x=0
+    while(n!=0):
+        x+=1
+        n=n//10
+    return x
 
-import numpy as np
-a=np.array([[1,2,3],[0,1,4]])
-print(a.ndim)
+def armstrong(n):
+    num=n
+    x=order(n)
+    s=0
+    while(n!=0):
+        d=n%10
+        s+=d**x
+        n=n//10
+    if num==s:
+        print("It is Armstrong number")
+    else:
+        print("Not Armstrong number")
+armstrong(int(input("Enter number of your choice")))
